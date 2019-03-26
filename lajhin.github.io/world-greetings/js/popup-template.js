6a382b920b4cff67aca042fe05051acfab29038c
lajhin <47068899+lajhin@users.noreply.github.com>
2019-03-26 14:32:44 -0400

    newest github site with project 2 all
    wireframe, final site, process blog

---
+++ lajhin.github.io/world-greetings/js/popup-template.js
@@ -0,0 +1,117 @@
+
+// Button to open a modal
+$(document).ready(function (){
+    $('#tibet-face').click(function() {
+        $("#tibet-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+//Button to close a modal
+$(document).ready(function (){
+    $('#tibet-close').click(function() {
+        $("#tibet-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+
+
+
+$(document).ready(function (){
+    $('#korea-face').click(function() {
+        $("#korea-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#korea-close').click(function() {
+        $("#korea-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+
+
+
+
+$(document).ready(function (){
+    $('#brazil-face').click(function() {
+        $("#brazil-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#brazil-close').click(function() {
+        $("#brazil-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+
+
+
+
+$(document).ready(function (){
+    $('#newzealand-face').click(function() {
+        $("#newzealand-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#newzealand-close').click(function() {
+        $("#newzealand-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+
+
+
+
+$(document).ready(function (){
+    $('#uganda-face').click(function() {
+        $("#uganda-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#uganda-close').click(function() {
+        $("#uganda-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+
+
+
+
+$(document).ready(function (){
+    $('#us-face').click(function() {
+        $("#us-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#us-close').click(function() {
+        $("#us-modal").toggleClass("visible");
+        $(".background-grey").toggleClass("visible");
+        });
+});
+
+
+
+
+
+
+//************************ Templates ************************
+
+// // Button to open a modal
+// $(document).ready(function (){
+//     //ID or Class name of the button goes into the first section
+//     $('#place-button-name-here').click(function() {
+//         //ID or Class name of the modal you want to appear on click goes into this second area.
+//         $("#place-modal-name-here").toggleClass("visible");
+//         });
+// });
+//
+// // Button to close a modal
+// $(document).ready(function (){
+//     $('#place-modal_close-icon-name-here').click(function() {
+//         $("#place-modal-name-here").toggleClass("visible")
+//         });
+// });
